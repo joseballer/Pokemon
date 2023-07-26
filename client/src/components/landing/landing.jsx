cfbe6f2ff72cc4c9df2cd2e0a511fe918650047c
Joseballer <joseballera@gmail.com>
2023-07-26 18:55:26 -0400

    primeros pasos front

---
+++ client/src/components/landing/landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './landing.css';
+
+
+
+
+const Landing = () => {
+
+    return (
+        <div className="landing">
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Landing;
